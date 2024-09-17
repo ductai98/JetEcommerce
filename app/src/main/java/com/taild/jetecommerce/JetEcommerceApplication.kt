0f35fe5269982b91142ac8761da77b42ace38f0a
ductai <lductai98@gmail.com>
2024-09-17 15:06:27 +0700

    Create Project and add hilt, retrofit dependency

---
+++ app/src/main/java/com/taild/jetecommerce/JetEcommerceApplication.kt
@@ -0,0 +1,8 @@
+package com.taild.jetecommerce
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetEcommerceApplication : Application() {
+}
---
+++ app/src/main/java/com/taild/jetecommerce/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.taild.jetecommerce.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
